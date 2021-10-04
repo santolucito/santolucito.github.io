cd48c80a87fe1f3fe39343ec7b948b7cb440ad13
Mark Santolucito <mark.santolucito@yale.edu>
2021-10-04 09:17:45 -0400

    allow more samples

--- COMS3430/fall2021/phaser/script.js
+++ COMS3430/fall2021/phaser/script.js
@@ -24,17 +24,54 @@ function makeBiquad(){
   biquadFilter.Q.setValueAtTime(2, audioCtx.currentTime);
   return biquadFilter;
 }
-const playButton = document.querySelector('button');
 
+const sourceSelector = document.getElementById('audio')
+sourceSelector.addEventListener('change', function (event) {
+  console.log(event.target.value);
+  if(event.target.value == "Barnard"){
+    sourceHilltop.connect(sourceSel);
+    sourceDisco.disconnect();
+    sourceString.disconnect();
+
+  }
+  else if (event.target.value == "Disco") {
+    sourceDisco.connect(sourceSel);
+    sourceHilltop.disconnect();
+    sourceString.disconnect();
+  }
+  else{
+    sourceString.connect(sourceSel);
+    sourceHilltop.disconnect();
+    sourceDisco.disconnect();
+
+  }
+}, false);
+
+const playButton = document.querySelector('button');
 playButton.addEventListener('click', async function() {
 
   audioCtx = new AudioContext()
 
+  sourceSel = audioCtx.createGain();
+
+  discoSource = await loadBuffer('../../fall2020/samples/disco.wav');
+  sourceDisco = audioCtx.createBufferSource();
+  sourceDisco.loop = true;
+  sourceDisco.connect(sourceSel);
+  sourceDisco.buffer = discoSource;
+  sourceDisco.start();
+
+  hilltop = await loadBuffer('../../fall2020/samples/barnard.mp3');
+  sourceHilltop = audioCtx.createBufferSource();
+  sourceHilltop.loop = true;
+  sourceHilltop.buffer = hilltop;
+  sourceHilltop.start();
+
   var audioBuffer = await loadBuffer('../../fall2020/samples/string.wav');
-  const source = audioCtx.createBufferSource();
-  source.loop = true;
-  source.buffer = audioBuffer;
-  source.start();
+  sourceString = audioCtx.createBufferSource();
+  sourceString.loop = true;
+  sourceString.buffer = audioBuffer;
+  sourceString.start();
 
   bi1 = makeBiquad();
   bi2 = makeBiquad();
@@ -56,8 +93,8 @@ playButton.addEventListener('click', async function() {
   lfo.connect(lfoGain).connect(bi3.frequency);
   lfo.connect(lfoGain).connect(bi4.frequency);
 
-  source.connect(dry).connect(audioCtx.destination);
-  source.connect(wet).connect(bi1).connect(bi2).connect(bi3).connect(bi4).connect(audioCtx.destination);
+  sourceSel.connect(dry).connect(audioCtx.destination);
+  sourceSel.connect(wet).connect(bi1).connect(bi2).connect(bi3).connect(bi4).connect(audioCtx.destination);
 
 });
 
