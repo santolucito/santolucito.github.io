118e8555abf8e3537ea275acd8f8deb4b17da5a2
Mark Santolucito <msantolu@barnard.edu>
2022-09-07 18:11:52 -0400

    nicer slider

---
+++ COMS3430/fall2022/listener_slider/main.js
@@ -0,0 +1,35 @@
+
+var audioCtx;
+var osc;
+const playButton = document.querySelector('button');
+
+function initAudio() {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)
+    osc = audioCtx.createOscillator();
+    osc.connect(audioCtx.destination);
+    osc.start()
+}
+
+var slider = document.getElementById("pitch");
+
+slider.addEventListener("change", function (val) {
+    osc.frequency.value = val;
+}, false);
+
+playButton.addEventListener('click', function () {
+
+    if (!audioCtx) {
+        initAudio();
+        return;
+    }
+    else if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
+    }
+    else if (audioCtx.state === 'running') {
+        audioCtx.suspend();
+    }
+
+    let pitch = document.getElementById("pitch").value
+    osc.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+
+}, false);
