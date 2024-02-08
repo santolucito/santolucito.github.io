1c78c91017057b855d716990997718f68ef4979c
Mark Santolucito <mark.santolucito@yale.edu>
2024-02-08 10:51:48 -0500

    add

--- COMS3930/spring2024/serialThree.js
+++ COMS3930/spring2024/serialThree.js
@@ -7,6 +7,7 @@ note that some pins don't work when connected to web serial
 -----
 
 #define BUTTON 35
+//this is the right button on the TTGO T1 Display
 
 void setup() {
   Serial.begin(115200);
@@ -68,36 +69,36 @@ async function readLoop() {
 var scene = new THREE.Scene();
 
 // Create a basic perspective camera
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 
 // Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({antialias:true});
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 
 // Configure renderer clear color
 renderer.setClearColor("#000000");
 
 // Configure renderer size
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Append Renderer to DOM
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
 // ------------------------------------------------
 // FUN STARTS HERE
 // ------------------------------------------------
 
 // Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-var cube = new THREE.Mesh( geometry, material );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: "#433F81" });
+var cube = new THREE.Mesh(geometry, material);
 
 // Add cube to Scene
-scene.add( cube );
+scene.add(cube);
 
 // Render Loop
 var render = function () {
-  requestAnimationFrame( render );
+  requestAnimationFrame(render);
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
